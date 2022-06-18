b64d5baae30712e4340a8febf9c6d80dd3daccbd
Rohan <rohgyas@hotmail.com>
2022-06-19 04:00:21 +0800

    Refactor screens with state to call composables directly

--- app/src/main/java/com/rrg/dinnerrecommendation/ui/recommendation/DrinkCategorySelection.kt
+++ app/src/main/java/com/rrg/dinnerrecommendation/ui/recommendation/DrinkCategorySelection.kt
@@ -3,13 +3,9 @@ package com.rrg.dinnerrecommendation.ui.recommendation
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.navigation.NavHostController
 import com.rrg.dinnerrecommendation.core.State
 import com.rrg.dinnerrecommendation.models.keys.RecommendationScreens
-import com.rrg.dinnerrecommendation.models.primary.DrinkCategory
 import com.rrg.dinnerrecommendation.ui.components.CircularIndeterminateProgressBar
 import com.rrg.dinnerrecommendation.ui.components.CocktailCategoryList
 import com.rrg.dinnerrecommendation.ui.components.NextButton
@@ -21,45 +17,27 @@ fun DrinkCategorySelection(
     navController: NavHostController,
     viewModel: RecommendationViewModel
 ) {
-    // val viewModel: RecommendationViewModel = hiltViewModel()
-
     LaunchedEffect(Unit) {
         viewModel.onEvent(RecommendationViewModel.RecommendationEvents.SearchCocktailCategories)
     }
 
-    val data: MutableState<List<DrinkCategory>> = remember {
-        mutableStateOf(listOf())
-    }
-
-    val isLoading: MutableState<Boolean> = remember {
-        mutableStateOf(true)
-    }
-
     when (val state = viewModel.stateDrinks.value) {
         is State.Loading -> {
-            isLoading.value = true
+            CircularIndeterminateProgressBar()
         }
         is State.LoadingFailed -> {
-            isLoading.value = false
             // TODO -- add error compose
         }
         is State.Loaded -> {
-            isLoading.value = false
-            data.value = state.data
+            CocktailCategoryList(
+                data = state.data,
+                viewModel = viewModel
+            )
+            NextButton(
+                onNextClick = { navController.safeNavigateTo(RecommendationScreens.FinalRecommendation.route) },
+                currentRecommendationScreen = RecommendationScreens.DrinkCategories,
+                viewModel = viewModel
+            )
         }
-    }
-
-    if (isLoading.value) {
-        CircularIndeterminateProgressBar()
-    } else {
-        CocktailCategoryList(
-            data = data.value,
-            viewModel = viewModel
-        )
-        NextButton(
-            onNextClick = { navController.safeNavigateTo(RecommendationScreens.FinalRecommendation.route) },
-            currentRecommendationScreen = RecommendationScreens.DrinkCategories,
-            viewModel = viewModel
-        )
     }
 }
--- app/src/main/java/com/rrg/dinnerrecommendation/ui/recommendation/MealCategorySelection.kt
+++ app/src/main/java/com/rrg/dinnerrecommendation/ui/recommendation/MealCategorySelection.kt
@@ -2,13 +2,9 @@ package com.rrg.dinnerrecommendation.ui.recommendation
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.navigation.NavHostController
 import com.rrg.dinnerrecommendation.core.State
 import com.rrg.dinnerrecommendation.models.keys.RecommendationScreens
-import com.rrg.dinnerrecommendation.models.primary.MealCategory
 import com.rrg.dinnerrecommendation.ui.components.CircularIndeterminateProgressBar
 import com.rrg.dinnerrecommendation.ui.components.MealCategoriesList
 import com.rrg.dinnerrecommendation.ui.components.MealCategoryInfoDialog
@@ -20,55 +16,37 @@ fun MealCategorySelection(
     navController: NavHostController,
     viewModel: RecommendationViewModel
 ) {
-    // val viewModel: RecommendationViewModel = hiltViewModel()
-
     LaunchedEffect(Unit) {
         viewModel.onEvent(RecommendationViewModel.RecommendationEvents.SearchMealCategories)
     }
 
-    val data: MutableState<List<MealCategory>> = remember {
-        mutableStateOf(listOf())
-    }
-
-    val isLoading: MutableState<Boolean> = remember {
-        mutableStateOf(true)
-    }
-
     when (val state = viewModel.stateMeals.value) {
         is State.Loading -> {
-            isLoading.value = true
+            CircularIndeterminateProgressBar()
         }
         is State.LoadingFailed -> {
-            isLoading.value = false
             // TODO -- add error compose
         }
         is State.Loaded -> {
-            isLoading.value = false
-            data.value = state.data
-        }
-    }
-
-    if (isLoading.value) {
-        CircularIndeterminateProgressBar()
-    } else {
-        MealCategoriesList(
-            data = data.value,
-            viewModel = viewModel
-        )
-        NextButton(
-            onNextClick = {
-                navController.safeNavigateTo(RecommendationScreens.DrinkCategories.route)
-            },
-            currentRecommendationScreen = RecommendationScreens.MealCategories,
-            viewModel = viewModel
-        )
-        val currentlyViewingMealDetails = viewModel.currentlyViewingMealDetails.value
-        if (currentlyViewingMealDetails != null) {
-            MealCategoryInfoDialog(
-                title = currentlyViewingMealDetails.strCategory,
-                text = currentlyViewingMealDetails.getCleanCategoryDescription()
-            ) {
-                viewModel.currentlyViewingMealDetails.value = null
+            MealCategoriesList(
+                data = state.data,
+                viewModel = viewModel
+            )
+            NextButton(
+                onNextClick = {
+                    navController.safeNavigateTo(RecommendationScreens.DrinkCategories.route)
+                },
+                currentRecommendationScreen = RecommendationScreens.MealCategories,
+                viewModel = viewModel
+            )
+            val currentlyViewingMealDetails = viewModel.currentlyViewingMealDetails.value
+            if (currentlyViewingMealDetails != null) {
+                MealCategoryInfoDialog(
+                    title = currentlyViewingMealDetails.strCategory,
+                    text = currentlyViewingMealDetails.getCleanCategoryDescription()
+                ) {
+                    viewModel.currentlyViewingMealDetails.value = null
+                }
             }
         }
     }
