9d28c96634bad8a8c61220cf8226d6ae5c34f8e8
Rohan <rohgyas@hotmail.com>
2022-06-22 17:55:17 +0800

    Ktlint formatting

--- app/src/main/java/com/rrg/dinnerrecommendation/ui/components/EmptyPlaceholder.kt
+++ app/src/main/java/com/rrg/dinnerrecommendation/ui/components/EmptyPlaceholder.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
